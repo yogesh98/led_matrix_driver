411219783c32eb54c324f7c1601811cf68027b4d
Yogesh <yogesh.patel0221@gmail.com>
2022-03-24 02:36:39 +0000

    (Update) weather

--- src/weather.py
+++ src/weather.py
@@ -4,27 +4,54 @@ from rgbmatrix import graphics
 import time
 import requests
 import os
+import json
+
 
 class CurrentWeather(Base):
     def __init__(self, *args, **kwargs):
         super(CurrentWeather, self).__init__(*args, **kwargs)
+        self.last_pull = None
+        self.weather = None
+
+    def get_weather(self):
+        if(self.last_pull is None or time.time() - self.last_pull >= 60):
+            apikey = os.getenv("OPENWEATHERAPIKEY")
+            lat = os.getenv("LAT")
+            lon = os.getenv("LON")
+            print("pulling data")
+            self.weather = requests.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+apikey).json()
+            print(self.weather)
+            self.last_pull = time.time()
+
 
     def run(self):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
+        
         font = graphics.Font()
-        font.LoadFont("../assets/fonts/7x13.bdf")
-        textColor = graphics.Color(255, 255, 255)
-        apikey = os.getenv("OPENWEATHERAPIKEY")
-        lat = os.getenv("LAT")
-        lon = os.getenv("LON")
-        print(apikey)
-        print("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+apikey)
-        weather = requests.get("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+apikey)
-        print(weather.json())
-        t_end = time.time() + 60 * 15
-        while True:
-            offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, 20, 20, textColor, "testing both")
+        font.LoadFont("../assets/fonts/helvR12.bdf")
 
-            time.sleep(0.01)
+        textColor = graphics.Color(255, 255, 255)
+
+        self.get_weather()
+        weather = self.weather['weather'][0]
+        main = self.weather['main']
+        main['temp'] = (float(main['temp']) - 273.15) * 9/5 + 32 
+        print("got weather")
+        img = Image.open('../assets/weather/'+weather['icon']+'.png')
+        print("open image")
+        img = img.convert('RGB')
+        print("convert image")
+        # # img.resize((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
+        print(self.weather)
+        while True:
+            # print("running")
+            offscreen_canvas.Clear()
+            offscreen_canvas.SetImage(img, -2, -6)
+
+            graphics.DrawText(offscreen_canvas, font, 4, 30, textColor, weather['main'])            
+            l = graphics.DrawText(offscreen_canvas, font, 32, 15, textColor, str(round(main['temp'],1)))
+            graphics.DrawText(offscreen_canvas, font, 32+l, 13, textColor, "°")
+
+            self.get_weather()
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+            time.sleep(0.01)
